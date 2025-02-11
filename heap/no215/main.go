322122850520e557243c693a288d06bc7db2dfb6
TojoMasahiko <jotonne55@gmail.com>
2025-02-11 17:21:51 +0900

    feat: imple no215

---
+++ heap/no215/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+func main() {
+	nums := []int{2, 1}
+	k := 1
+	fmt.Println(findKthLargest(nums, k))
+}
+
+func findKthLargest(nums []int, k int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	var h Intheap = Intheap(nums)
+	heap.Init(&h)
+
+	var x int
+	count := len(nums) - k
+	for h.Len() > 0 {
+		if count < 0 {
+			return x
+		}
+		x = heap.Pop(&h).(int)
+		count--
+	}
+	return -1
+}
+
+type Intheap []int
+
+func (h Intheap) Len() int {
+	return len(h)
+}
+
+func (h Intheap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h Intheap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *Intheap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *Intheap) Pop() any {
+	old := *h
+	x := old[len(old)-1]
+	*h = old[0 : len(old)-1]
+	return x
+}
